1ff7a9f187e8b4208bbe92eff1833af5a2e7ebfb
Philip <philipmjobe@gmail.com>
2021-12-16 13:36:56 -0500

    added Logout.vue and logic

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
@@ -14,6 +15,7 @@ const routes = [
   },
   { path: "/signup", name: "signup", component: Signup },
   { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
 
   {
     path: "/about",
