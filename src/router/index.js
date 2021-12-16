327ce2458c5ba5adc63107bd1f9b30e6185380f6
Philip <philipmjobe@gmail.com>
2021-12-16 13:35:17 -0500

    added Login.vue and logic

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -12,6 +13,8 @@ const routes = [
     component: Home,
   },
   { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+
   {
     path: "/about",
     name: "About",
