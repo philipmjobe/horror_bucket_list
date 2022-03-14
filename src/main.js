24ef16470d4bbb4d14ada78d1e1d215027f97212
Philip <philipmjobe@gmail.com>
2022-03-14 15:55:50 -0400

    updates main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "https://git.heroku.com/horrormoviebucketlist.git" : "/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://git.heroku.com/horrorbucketlist.git";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
