de4e899783eab2433ff0050fd6b275503fbbd27e
Philip <philipmjobe@gmail.com>
2022-04-19 14:42:36 -0400

    adds server.js and logic and added logic to scripts in package.json

--- src/router/index.js
+++ src/router/index.js
@@ -43,3 +43,7 @@ const router = new VueRouter({
 });
 
 export default router;
+
+// app.listen(process.env.PORT || 3000, function () {
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
