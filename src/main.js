6ccfc02cd0b35a34ae919d50c1ea3bf948884fbe
Philip <philipmjobe@gmail.com>
2022-04-21 16:16:53 -0400

    fixes defaults in main.js again

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://fathomless-tundra-47766.herokuapp.com/" : "/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://fathomless-tundra-47766.herokuapp.com/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
