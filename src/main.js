5001828042a25a8cd98a23362e8f6b070ae110dc
Philip <philipmjobe@gmail.com>
2022-04-21 15:21:43 -0400

    updates defaults in main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://horrorbucketlist.herokuapp.com" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://fathomless-tundra-47766.herokuapp.com/" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
