6b59ff32344b44bb50da389c2dcdaff9e4cf6e6b
Philip <philipmjobe@gmail.com>
2022-04-25 20:40:41 -0400

    removes base_url from index.js

--- src/router/index.js
+++ src/router/index.js
@@ -38,12 +38,8 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes,
 });
 
 export default router;
-
-// app.listen(process.env.PORT || 3000, function () {
-//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
