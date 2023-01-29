a0009a430fee886d19dd797854ed6555cdfd2ba0
Philip <philipmjobe@gmail.com>
2023-01-29 16:22:06 -0500

    fixes axios.default.baseURL in main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from "axios";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://capstoneprojectbackend-production.up.railway.app/";
+    : "https://capstoneprojectbackend-production.up.railway.app";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
